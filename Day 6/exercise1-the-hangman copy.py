4c705e941447319647c630f3e0ea6e3b161f458f
Bruna Moura Correia <mcorreia.bruna@gmail.com>
2021-09-05 22:16:46 -0300

    Completed the hangman challenge

---
+++ Day 6/exercise1-the-hangman copy.py
@@ -0,0 +1,45 @@
+from hangman_img import logo, stages
+import random
+word_list = ["sort", "reverse", "remove", "pop", "insert", "add", "index", "extend", "count", "copy", "clear", "append", "isnumeric", "type", "upper", "lower"]
+
+# randomly choose a word from the word_list
+word = random.choice(word_list)
+
+# to keep track of the remained chances
+lives = 5
+print(logo)
+print("Welcome to the Python Handman Game. You have 5 chances to get the hidden Python Method, good luck!")
+
+# blank spaces according to random word length
+display = []
+for letter in word:
+    display.append('_')
+
+while '_' in display:
+    guess = input("Guess a letter to save the hangman: \n").lower()
+    if guess in display:
+        print(f"Correct! You guessed the letter '{guess}'!")
+
+# to complete the blank spaces if the letter chosen bby the user matches the word        
+    for index, letter in enumerate(word):
+        if letter == guess:
+            display[index] = guess
+
+# if the letter chosen is not in the word, chances are reduced and hangman is updated 
+    if not guess in word:
+        lives -= 1
+        print(f"You've lost a life. There are {lives} chances left.")
+        print(stages[lives])
+    
+    # once the lives is 0 we lose and exit
+    if lives == 0:
+        print(f"You lose. The Python Method was {word}. Try again later.")
+        break
+
+    # print at each correct stage, join the elements into a string separated by spaces
+    print(f"{' '.join(display)}")
+
+# if we have some lives left and we got rid of all the _ we win!
+if lives != 0:
+    print('You WIN!')
+
