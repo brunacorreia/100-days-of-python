1f8923dd9e60fb6081faac480a515df0d3c49d5a
Bruna Moura Correia <mcorreia.bruna@gmail.com>
2021-09-08 15:27:15 -0300

    Added Day 8 - 'Cans of paint calculator'

---
+++ Day 8/exercise2-painting-calculator.py
@@ -0,0 +1,19 @@
+'''
+You are painting a wall. The instructions on the paint can says that 1 can of paint can cover 5 square meters of wall. 
+Given a random height and width of wall, calculate how many cans of paint you'll need to buy.
+'''
+
+import math
+
+def paint_calc(height, width, cover):
+  cans = math.ceil(test_h * test_w / coverage)
+  print(f"You will need {cans} cans of paint.")
+
+#Write your code above this line 👆
+# Define a function called paint_calc() so that the code below works.   
+
+# 🚨 Don't change the code below 👇
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
