b436e54948b8bdaa0c04cb9ecacfd54f193a34a2
Bruna Moura Correia <mcorreia.bruna@gmail.com>
2021-09-09 17:19:44 -0300

    Joint similar functions to obtain cleaner code

--- Day 8/finalproject-caeser-cipher.py
+++ Day 8/finalproject-caeser-cipher.py
@@ -3,25 +3,17 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
-shift %= 26
 
-def encrypt(plain_text, shift_amount):
-    encrypted_text = ""
-    for letter in plain_text:
+def caesar(text, shift, direction):
+    converted_text = ""
+    for letter in text:
         current_index = alphabet.index(letter)
-        new_index = current_index + shift_amount
-        encrypted_text += alphabet[new_index]
-    print(f"The encoded text is {encrypted_text}")
-
-def decrypt(encrypted_text, shift_amount):
-    plain_text = ""
-    for letter in encrypted_text:
-        current_index = alphabet.index(letter)
-        new_index = current_index - shift_amount
-        plain_text += alphabet[new_index]
-    print(f"The decoded text is {plain_text}")
-
-if direction == "encode":
-    encrypt(plain_text=text, shift_amount=shift)
-elif direction == "decode":
-    decrypt(encrypted_text=text, shift_amount=shift)
+        if direction == "encode":
+            new_index = (current_index + shift) % 26
+        elif direction == "decode":
+            new_index = (current_index - shift) % 26
+        else:
+            print("Invalid command.")
+        converted_text += alphabet[new_index]
+    print(f"The {direction}d text is {converted_text}")
+caesar(text, shift, direction)
